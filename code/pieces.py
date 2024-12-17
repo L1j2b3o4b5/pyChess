d6491ae926c3c708c9d0aed0588c26eaa1f21125
L1j2b3o4b5 <ljbob999@gmail.com>
2024-12-16 18:24:25 -0800

    Update pieces.py

--- code/pieces.py
+++ code/pieces.py
@@ -1,7 +1,34 @@
 from .chess import Piece
 
-class King(Piece):
-  def __init__(self, color):
+# Error codes: 0 = nothing, 1 = invalid, 2 = against the rules
+
+# Pieces
+class PieceTemplate(Piece):
+  def __init__(self, name, whStartX, whStartY, bkStartX, bkStartY, color):
     if color == "white":
-      super.__init__("king", 
+      super.__init__(name, whStartX, whStartY, color)
+    elif color == "black":
+      super.__init__(name, bkStartX, bkStartY, color)
+
+  def move(self, movX, movY, board):
+    # if <enter condition here>:
+    super.move(movX, movY, board)
+
+  # Enter other special moves here
+class King(PieceTemplate):
+  def __init__(self, color):
+    super.__init__(
+
+  def move(self, movX, movY, board):
+    if (abs(movX) < 2) and (abs(movY) < 2):
+      return super.move(movX, movY, board)
+    else:
+      return 2
+
+  def castle(self, board):
+    if not self.moved and board:
+      
+    
+    
+  
     
